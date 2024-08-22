7425f29f77dbdb7b7fa04bb5735867eff8992bc2
jansneeuw <sven@zwet.net>
2024-08-22 14:02:20 +0200

    init

--- client/src/boot/index.js
+++ client/src/boot/index.js
@@ -2,5 +2,6 @@
 import registerComponents from 'boot/registerComponents';
 
 window.document.addEventListener('DOMContentLoaded', () => {
+  console.log('registering components');
   registerComponents();
 });
